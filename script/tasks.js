d012158ec8cf261a0a58309b69f95f2f16cde269
Sami Konzo <samikonzo@gmail.com>
2017-10-09 15:37:37 +0300

    make lazy task: done

--- script/tasks.js
+++ script/tasks.js
@@ -2,22 +2,11 @@
 var l = console.log;
 
 
-function ranks(arr) {
-	var currentPlace = 1,
-		placeObj = {};
-
-	arr.slice().sort( (a,b) => b - a).forEach(item => {
-		if(!placeObj[item]) placeObj[item] = currentPlace
-		currentPlace++
-	})
-
-	return arr.map(item => {
-		return placeObj[item];
-	})
-}
-
-
-
-
-l(ranks([9,3,6,10]))
-l(ranks([3,3,3,3,3,5,1]))
+var make_lazy = function (f) {
+  var args = [].slice.call(arguments,1);
+  var result = f.apply(null, args);
+  
+  return function(){
+    return result
+  }
+};
