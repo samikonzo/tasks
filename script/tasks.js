f2f0d5c771520d8f1640ca2c6691ffe66ebfa6af
Sami Konzo <samikonzo@gmail.com>
2017-10-09 15:49:28 +0300

    chain evolution: done

--- script/tasks.js
+++ script/tasks.js
@@ -1,12 +1,27 @@
 'use strict'
 var l = console.log;
 
-
-var make_lazy = function (f) {
-  var args = [].slice.call(arguments,1);
-  var result = f.apply(null, args);
-  
-  return function(){
-    return result
-  }
+Number.prototype.add = function(n) {
+	return this + n
 };
+
+Number.prototype.subtract = function(n) {
+	return this - n
+};
+
+Number.prototype.multiply = function(n) {
+	return this * n
+};
+Number.prototype.divide = function(n) {
+	return this / n
+};
+Number.prototype.square = function() {
+	return this * this
+};
+
+l((0).add(2))
+l((5).subtract(1))
+l((3).multiply(2))
+l((10).divide(5))
+l((5).square())
+l((3).add(5).multiply(2))
