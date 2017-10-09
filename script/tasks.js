a7997ae1a0fb6399302601665c2dfb766eeeba23
Sami Konzo <samikonzo@gmail.com>
2017-10-09 15:31:48 +0300

    ranks task: done

--- script/tasks.js
+++ script/tasks.js
@@ -1,26 +1,23 @@
 'use strict'
 var l = console.log;
 
-function compareVersions(version1, version2) {
-	var v1 = version1.split('.'),
-		v2 = version2.split('.');
 
-	for(var i = 0; i < v1.length; i++){
-		if(v2[i] == undefined) return true
-		if(+v1[i] > +v2[i]) return true	
-		if(+v2[i] > +v1[i]) return false
-	}	
+function ranks(arr) {
+	var currentPlace = 1,
+		placeObj = {};
 
-	if(v2.length > v1.length) return false;
+	arr.slice().sort( (a,b) => b - a).forEach(item => {
+		if(!placeObj[item]) placeObj[item] = currentPlace
+		currentPlace++
+	})
 
-	return true
+	return arr.map(item => {
+		return placeObj[item];
+	})
 }
 
 
 
-l(compareVersions("11", "10"));
-l(compareVersions("11", "11"));
-l(compareVersions("10.4.6", "10.4"));
-l(compareVersions("10.4", "11"));
-l(compareVersions("10.4", "10.10"));
-l(compareVersions("10.4.9", "10.5"));
+
+l(ranks([9,3,6,10]))
+l(ranks([3,3,3,3,3,5,1]))
